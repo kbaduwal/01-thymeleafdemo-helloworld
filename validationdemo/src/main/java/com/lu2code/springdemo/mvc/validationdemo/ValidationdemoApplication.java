b7b1570a04488c123f6d7d18dfbadbafa2f1eee0
kbaduwal <khakendra1999@gmail.com>
2023-12-29 13:19:07 +0530

    Spring MVC Validatio

---
+++ validationdemo/src/main/java/com/lu2code/springdemo/mvc/validationdemo/ValidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.lu2code.springdemo.mvc.validationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
